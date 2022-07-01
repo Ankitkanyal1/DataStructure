40e283d0d9724186fdf8cf6e0ca2f7d12a8320c1
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-01 21:26:19 +0530

    Update july_1_2022(i).java

--- july_1_2022(i).java
+++ july_1_2022(i).java
@@ -20,3 +20,34 @@ class Solution {
         return ans;
     }
 }
+
+
+
+//Approach 2=MaxLeft& MaxRight arrray
+
+class Solution {
+    public int trap(int[] height) {
+        int l=height.length,ans=0;
+        int []left=new int[l];
+        int []right=new int[l];
+        left[0]=height[0];
+        right[l-1]=height[l-1];
+        for(int i=0;i<l;i++){
+            if(i==0){
+                left[0]=height[0];
+                right[l-1]=height[l-1];
+            }
+            else{
+                left[i]=Math.max(height[i],left[i-1]);
+                right[l-i-1]=Math.max(height[l-i-1],right[l-i]);
+            }
+        
+        }
+        
+        for(int i=0;i<l;i++){
+            ans+=Math.min(left[i],right[i])-height[i];
+        }
+        
+        return ans;
+    }
+}
