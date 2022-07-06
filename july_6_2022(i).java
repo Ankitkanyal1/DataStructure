8dbeaa5fcb7d3829cac9fc16674e74d9ddc58b3f
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-06 18:36:19 +0530

    Create july_6_2022(i).java

---
+++ july_6_2022(i).java
@@ -0,0 +1,12 @@
+// Brute force Approach
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+                if(nums[i]==nums[i+1])
+                return true;
+            }
+        return false;
+    }
+}
