26bfd811cc5598111e594fd8cde7b2c50a62b0c6
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-07 14:51:15 +0530

    Two Sum
    Two Sum

---
+++ july_7_2022(i).java
@@ -0,0 +1,16 @@
+// Approach 1 ==> Brute force 
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int []temp=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++)
+            if(nums[i]==target-nums[j]){
+               temp[0]=i;
+               temp[1]=j;
+            }
+                
+        }
+        return temp;
+    }
+}
