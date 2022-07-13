57ae825fbc9c6445e2f0bcdab20a005ec2e1b18b
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-13 20:41:04 +0530

    Update july_13_2022(ii).java

--- july_13_2022(ii).java
+++ july_13_2022(ii).java
@@ -32,3 +32,15 @@ class Solution {
 
 // Approach 2 ==> Recursive approach
 // Time Complexity ==> 0(n)
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) {
+            return head;
+        }
+     ListNode newHead=reverseList( head.next);
+        head.next.next=head;
+        head.next=null;
+        return newHead;
+    }
+}
