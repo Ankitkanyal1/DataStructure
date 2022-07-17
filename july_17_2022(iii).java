12aec8cac4c8e16a8cc2dd5ec2c111bb95b8d5c4
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-17 20:19:06 +0530

    Maximum Depth of Binary Tree

---
+++ july_17_2022(iii).java
@@ -0,0 +1,26 @@
+// Time Complexity ==> O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int lr=maxDepth(root.left);
+        int rr=maxDepth(root.right);
+        int ans=Math.max(lr,rr);
+        return ans+1;
+    }
+}
