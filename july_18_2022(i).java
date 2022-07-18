e413dba3565cab378cb6f5b0b132514e3dd48ec3
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-18 23:03:47 +0530

    Invert Binary Tree

---
+++ july_18_2022(i).java
@@ -0,0 +1,27 @@
+// Time Complexity ==> O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return root;
+        TreeNode ln=invertTree(root.left);
+        TreeNode rn=invertTree(root.right);
+        root.left=rn;
+        root.right=ln;
+        return root;
+    }
+}
