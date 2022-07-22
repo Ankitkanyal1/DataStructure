aa82e85a7f9fa27777e53fd8e98d392e1560ac83
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-22 09:06:37 +0530

    Rotate Array

--- july_22_2022(i).java
+++ july_22_2022(i).java
@@ -15,3 +15,26 @@ class Solution {
            array[0] = temp;      
     }
 }   
+
+
+//Approach 2
+// Time Complexity ==> 
+class Solution {
+    public void rotate(int[] nums, int k){
+        if(k>nums.length){
+            k%=nums.length;
+        }
+        int ans[]=new int[nums.length];
+        int n=nums.length;
+        for(int i=0;i<k;i++){
+            ans[i]=nums[n-k+i];
+        }
+        for(int i=k;i<n;i++){
+            ans[i]=nums[i-k];
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=ans[i];
+        }
+        
+    }
+}
