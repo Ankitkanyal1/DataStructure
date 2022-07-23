d038f5200f4c1c482c7cc31c1c53bff6af7eb0e5
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-23 08:28:25 +0530

    Merge Intervals

--- july_23_2022(i).java
+++ july_23_2022(i).java
@@ -21,3 +21,27 @@ class Solution {
         return list.toArray(new int[list.size()][]);
     }
 }
+
+
+// Approach 2==> Sorting & Two pointer || Time Complexity==>O(nlogn)+O(n) 
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> ans=new ArrayList<>();
+        Arrays.sort(intervals,(a,b) ->Integer.compare(a[0],b[0]));
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        for(int[] i:intervals){
+            if(i[0]<=end){
+                end=Math.max(end,i[1]);
+            }
+            else{
+                ans.add(new int[]{start,end});
+                start=i[0];
+                end=i[1];
+            }
+        }
+        ans.add(new int[]{start,end});
+        return ans.toArray(new int[0][]);
+    }
+}
