fdb083e6c1864243e941ec9ef2f75ec6700710b7
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-28 07:23:30 +0530

    Subarray Sum Equals K

---
+++ july_28_2022(ii).java
@@ -0,0 +1,17 @@
+// Approach 1 ==> Brute Force  || Time Complexity ==> O(n^2)  Not Recommended
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n=nums.length;
+        int ans=0;
+        for(int i =0; i<n; i++){
+            int sum = 0;
+            for(int j = i; j<n;j++){
+                sum+=nums[j];
+                System.out.println(sum+",");
+                if(sum==k) ans++;
+            }      
+        }
+        return ans;
+    }
+}
