ab0c9056d682c7683834479bbf8ddd3c779ba1e4
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-08-07 07:30:49 +0530

    Excel Sheet Column Number

--- (61)7Aug2022.java
+++ (61)7Aug2022.java
@@ -16,3 +16,16 @@ class Solution {
 
 Input: columnTitle = "A"
 Output: 1
+    
+// Approach 2 ==> Using ASCII || Time Complexity ==> O(N)
+    
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int ans=0;
+        for(int i=0;i<columnTitle.length();i++){
+            ans*=26;
+            ans+=columnTitle.charAt(i)-'@';   // ASCII value of '@' is 64 and 'A' is 65
+        }
+        return ans;
+    }
+}
