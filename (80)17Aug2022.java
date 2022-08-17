9e17c2cf792d2c6272f64976e2b4d047d1d7fcf0
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-08-17 13:59:48 +0530

    Linked List Cycle

---
+++ (80)17Aug2022.java
@@ -0,0 +1,32 @@
+// Approach 1 ==> Two Pointer Approach || Time Complexity ==> O(N)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null) return false;
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
