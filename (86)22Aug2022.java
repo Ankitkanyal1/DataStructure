cb307b2a62d2936c62fa2a0b450462a21ff6a783
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-08-23 08:33:23 +0530

    Find the Duplicate Number

--- (86)22Aug2022.java
+++ (86)22Aug2022.java
@@ -26,4 +26,18 @@ class Solution {
     }
 }
 
-// Approach 3 ==> 
+// Approach 3 ==> Time Complexity ==> O(N) || Space Complexity ==> O(1)
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            index=Math.abs(nums[i])-1;
+            if(nums[index]<0){
+                return Math.abs(nums[i]);
+            }
+            nums[index]=-nums[index];
+        }
+        return -1;
+    }
+}
