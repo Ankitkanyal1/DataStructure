3813ffa32262f9bbb44527d82df3b75e768ac63a
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-09-28 22:32:15 +0530

    Update (104)28Sep2022.java

--- (104)28Sep2022.java
+++ (104)28Sep2022.java
@@ -15,3 +15,45 @@ class Solution {
 Output: [1,0,2,1]
 Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]*/
 
+// Approach 2 ==> Bitmanipulation || Time Complexity ==> O(N)
+
+class Solution {
+    public int numberOfSteps(int num) {
+        int steps=0;
+        while(num!=0){
+           num=(num&1)==1?num^1:num>>1;
+            steps++;
+        }
+        return steps;
+    }
+}
+
+// Approach 3 ==> Iterative 
+
+class Solution {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num > 0) {
+            num = num % 2 == 0 ? num / 2 : num - 1;
+            count++;
+        }
+        return count;
+    }
+}
+
+// Approach 4 ==> Recursive 
+
+class Solution {
+    public int numberOfSteps(int num) {
+        return count(num, 0);
+    }
+    private int count(int num, int steps) {
+        if (num == 0) {
+            return steps;
+        }
+        if (num % 2 == 0) {
+            return count(num / 2, steps + 1);
+        }
+        return count(num - 1, steps + 1);
+    }
+}
