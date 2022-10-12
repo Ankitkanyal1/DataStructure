b0a1073beccdbda522f91dad900796310de715ab
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-10-12 22:11:25 +0530

    Array Partition

---
+++ (115)12Oct2022.java
@@ -0,0 +1,21 @@
+// Approach 1 ==> Sorting || Time Complexity ==> O(NlogN)
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length;i=i+2){
+            sum=sum+nums[i];
+        }
+        return sum;
+    }
+}
+
+/*Input: nums = [1,4,3,2]
+Output: 4
+Explanation: All possible pairings (ignoring the ordering of elements) are:
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4.*/
+
