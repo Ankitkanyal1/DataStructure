1eb29098c41cceadb5a7845df0926e7dd2bc468c
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2023-03-01 08:13:45 +0530

    Unique Paths

---
+++ (122)1March2023.java
@@ -0,0 +1,23 @@
+/*  Unique Paths
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+*/
+
+// Approach 1 ==> Using DP
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0)  dp[i][j]=1;
+                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
